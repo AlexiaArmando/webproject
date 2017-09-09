074bc5089b11b3c6dede9d20500b14b24e333636
unknown <alexiaarmando04@gmail.com>
2017-09-09 13:33:12 +0800

    initial file

---
+++ helloworld.php
@@ -0,0 +1,14 @@
+<html>
+<head>
+	<title>Hello World App</title>
+</head>
+<body>
+	<?php
+		for($ctr=1;$ctr<=6;$ctr++){
+			echo "<h$ctr>Hello World!</h$ctr>";
+		}
+		//git init
+		//three trees >1. Working directory (add) 2. index (commit) 3. HEAD
+	?>
+</body>
+</html>
